fff5d32fd610fb4faf40d22a1af106d0cac95f8e
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-02-15 18:45:10 +0900

    messageコントローラーの作成とルートパスの設定、

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,10 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
