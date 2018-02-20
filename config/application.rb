216131a98d1bf21c34ad198a816fc85b430e81eb
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-02-20 17:51:40 +0900

    ユーザー情報編集機能の追加, フラッシュメッセージの表示機能の実装

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,6 @@ module ChatSpace
       g.helper false
       g.test_framework false
     end
+    config.i18n.default_locale = :ja
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'messages#index'
+  resources :users, only: [:edit, :update]
 
 end
