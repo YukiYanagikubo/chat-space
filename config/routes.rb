9ddc30ce7ecbbf9b144a24cd279df245cac82c09
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-02-21 14:32:01 +0900

    グループの編集機能の実装とルートパスの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-
   devise_for :users
-  root 'messages#index'
-  # root 'groups#index'
-  resources :users, only: [:edit, :update]
+  root 'groups#index'
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
+    resources :messages, only: [:index]
   end
 end
