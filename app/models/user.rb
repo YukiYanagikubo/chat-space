fda1f920f0770f4840552d5bc0b9a2fc8d8ba8e6
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-02-21 12:52:49 +0900

    userへアソシエーションを追加、グループをサイドバーに表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :groups, through: :members
+  has_many :members
+  has_many :messages
 end
