a50599270abbab7c3328e0eb2a143d696ecbb009
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-02-21 20:06:28 +0900

    メッセージ送信機能の実装

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
   gem 'erb2haml'
   gem "font-awesome-rails"
   gem 'devise'
+  gem 'carrierwave'
+  gem 'mini_magick'
--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,12 @@ class Group < ApplicationRecord
   has_many :messages
   has_many :users, through: :members
   validates :name, presence: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
