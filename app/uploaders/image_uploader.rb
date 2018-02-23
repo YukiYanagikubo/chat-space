d069098de4aba0e04765b5d4209cfbf211bbd6b5
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-02-23 21:24:48 +0900

    表示バグの修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [400, 400]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
