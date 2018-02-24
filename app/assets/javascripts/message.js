c18baa708c384185a729afb83587b9b63c972231
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-02-24 14:55:24 +0900

    イベントが発火したときにAjaxを使用して、messages#createが動くようにする

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    console.log(this)
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+})
