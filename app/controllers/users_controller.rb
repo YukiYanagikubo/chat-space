3d0204c5e188f7058376f97b328bcc215e34c862
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-02-26 11:36:39 +0900

    formatよって処理を分ける

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
 
   def index
     @users = User.where('name LIKE(?)', "%#{params[:name]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def edit
