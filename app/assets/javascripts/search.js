69aa0c87b303f0d4016330c4d81fa900823a3a1f
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-02-27 12:01:22 +0900

    インデントと引数の名前の修正

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,21 +1,21 @@
 $(function() {
 
-var search_list = $("#user-search-result");
+  var search_list = $("#user-search-result");
 
-function appendUserName(users) {
-  var html = `<div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">${users.name}</p>
-                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${users.id}" data-user-name="${users.name}">追加</a>
-              </div>`
-  search_list.append(html);
-}
+  function appendUserName(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>`
+    search_list.append(html);
+  }
 
-function appendNoUserName(users) {
-  var html = `<p>
-                <div class="chat-group-user__name'>${ users }</div>
-              </p>`
-  search_list.append(html);
-}
+  function appendNoUserName(fail_comment) {
+    var html = `<p>
+                  <div class="chat-group-user__name'>${fail_comment}</div>
+                </p>`
+    search_list.append(html);
+  }
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
@@ -42,15 +42,15 @@ function appendNoUserName(users) {
     }
   });
 
-var search_list_add = $("#chat-group-users");
+  var search_list_add = $("#chat-group-users");
 
-function appendUserNameAdd(user_name, user_id) {
-   var html =`<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-                <input name='group[user_ids][]' type='hidden' value='${user_id}'>
-                <p class='chat-group-user__name'>${user_name}</p>
-                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-              </div>`
-    search_list_add.append(html);
+  function appendUserNameAdd(user_name, user_id) {
+     var html =`<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                  <p class='chat-group-user__name'>${user_name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+      search_list_add.append(html);
   }
 
   $("#user-search-result").on("click", ".chat-group-user__btn--add", function () {
