4f69920205a4ef09215f99fe7d15f908bd0db2bf
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-02-28 00:41:38 +0900

    Revert "グループ作成できないバグの修正"
    This reverts commit 1b25c76d2bd5a7964d02caa2c589ce90f56b378a.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,7 +8,6 @@ before_action :set_group, only: [:edit, :update]
 
   def create
     @group = Group.new(group_params)
-    @group.users << current_user
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
