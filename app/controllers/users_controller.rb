cc7b91b06497e1daebccc8f3f54c13dc6d751649
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-02-27 12:34:54 +0900

    where.notを使わずに表現

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:name]}%").where.not(id:current_user)
+    @users = User.where('name LIKE(?)', "%#{params[:name]}%" ).where("id NOT IN (#{current_user.id})")
     respond_to do |format|
       format.html
       format.json
