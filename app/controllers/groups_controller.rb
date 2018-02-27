1b25c76d2bd5a7964d02caa2c589ce90f56b378a
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-02-27 16:44:34 +0900

    グループ作成できないバグの修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,6 +8,7 @@ before_action :set_group, only: [:edit, :update]
 
   def create
     @group = Group.new(group_params)
+    @group.users << current_user
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
