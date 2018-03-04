971eef6da725895b428c99ce9f2f6e18d4732696
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-03-04 12:34:54 +0900

    テストコードで使用する画像の位置の変更

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :message do
     content Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/uploads/message/image/2/neko.jpeg")
+    image File.open("#{Rails.root}/app/assets/images/neko.jpeg")
     user
     group
   end
